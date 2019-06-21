405dd609caa743332a2c7c59ca280e1de8ecb6e6
Tish Chungoora <tishal82@gmail.com>
2019-06-21 21:33:24 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,34 @@
+// Experiment with 'for' loop
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+    array.push("I am 1 strange loop.");
+    } else {
+    array.push(`I am ${i} strange loops.`);
+    }
+  }
+  return array;
+}
+
+// Experiment with 'while' loop
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return "done";
+}
+
+// Experiment with 'do-while' loop
+
+function doWhileLoop(num) {
+  function incrementVariable() {
+  var i = i + 1;
+  return i;
+  }
+  do { 
+    console.log("I run once regardless.");
+  } while (incrementVariable() < num);
+}
+
